2229f0ea26b8c4f8f8e31e6d1d147284e6587195
Michael Porter <michaelporter.chemistry@gmail.com>
2018-09-20 10:50:22 -0500

    rewriting sql to be sync instead of async. it isn't the "node way" but it makes more sense to me and it is easier to queue up lots of inserts to do at once rather than one at a time

--- routes/api.js
+++ routes/api.js
@@ -1,50 +1,13 @@
 let unimod = require('js-unimod');
 let express = require('express');
 let router = express.Router();
-let mysql = require('mysql');
-let syncMysql = require('sync-mysql');
+let mysql = require('sync-mysql'); // Asynchronicity can shove it for now. Maybe at some point I'll make the SQL stuff async
 let fastaParser = require('fasta-js');
 let peptideCutter = require('peptide-cutter');
-let Q = require('q');
 
 const BATCH_SIZE = 5000;
 
-function addOrganism(organism) {
-  let deferred = Q.defer();
-  console.log(organism);
-  deferred.resolve(1);
-  return deferred.promise;
-}
-
-function addProtein(protein) {
-  let deferred = Q.defer();
-  console.log(protein);
-  deferred.resolve("success");
-  return deferred.promise;
-}
-
-async function runCreationCommand(connection, cmd) {
-  await connection.query(cmd, function (error, results, fields) {
-    if (error) return false;
-    return true;
-  });
-}
-
-function cleanUp(connection, res) {
-  let return_status = {};
-  connection.end(function(err) {
-    if (err) {
-      return_status.msg = "Error closing connection";
-      return_status.status = 400;
-      return return_status;
-    }
-    console.log("\nDone!\n");
-    return_status.msg = 'Process complete';
-    return_status.status = 200;
-    res.send(return_status);
-  });
-}
-
+/*
 function processFasta(connection, data, res, dbEnzymes, dbMods) {
   let deferred = Q.defer();
   let fastaOptions = {
@@ -130,7 +93,7 @@ function createEntries(res, connection, data, sequenceData, shouldCreateDecoy, d
             return;
           }
           seqID = seqResults['insertId'];
-          
+
 
 
 
@@ -150,14 +113,11 @@ function createEntries(res, connection, data, sequenceData, shouldCreateDecoy, d
 
   return deferred.promise;
 }
+*/
 
 router.post('/add', function(req, res, next) {
-  let return_status = {
-    msg: 'Unknown failure',
-    status: 400
-  };
 
-  var connection = mysql.createConnection({
+  let connection = new mysql({
     host     : req.body.server_address,
     port     : req.body.server_port,
     user     : req.body.server_username,
@@ -165,14 +125,7 @@ router.post('/add', function(req, res, next) {
     database : 'pepDB'
   });
 
-  let syncConnection = new syncMysql({
-    host     : req.body.server_address,
-    port     : req.body.server_port,
-    user     : req.body.server_username,
-    password : req.body.server_password,
-    database : 'pepDB'
-  });
-  let dbEnzymesRaw = syncConnection.query('SELECT * from enzymes;');
+  let dbEnzymesRaw = connection.query('SELECT * from enzymes;');
   let dbEnzymes = {};
   for(let i=0; i<dbEnzymesRaw.length; i++) {
     let name = dbEnzymesRaw[i].enzyme;
@@ -180,7 +133,7 @@ router.post('/add', function(req, res, next) {
     dbEnzymes[name] = id;
   }
 
-  let dbModsRaw = syncConnection.query('SELECT * from unimod;');
+  let dbModsRaw = connection.query('SELECT * from unimod;');
   let dbMods = {};
   for(let i=0; i<dbModsRaw.length; i++) {
     let name = dbModsRaw[i].name;
@@ -188,38 +141,20 @@ router.post('/add', function(req, res, next) {
     dbMods[name] = id;
   }
   
-  connection.connect(function(err) {
-    if (err) {
-      return_status.msg = "Error connecting to MySQL database: "+err.stack;
-      res.send(return_status);
-      return;
-    }
-    console.log('connected as id ' + connection.threadId);
-
-
-
-    // Add the organism
-    let organismSQL = 'INSERT INTO organisms (organism) VALUES(?)';
-    connection.query(organismSQL, [req.body.organismName], function(error, results, fields) {
-      if(error) {
-        return_status.msg = "Error adding organism";
-        res.send(return_status);
-        return;
-      };
-      req.body.organismID = results['insertId'];
+  // Add the organism
+  let organismSQL = 'INSERT INTO organisms (organism) VALUES(?)';
+  let organismResults = connection.query(organismSQL, [req.body.organismName]);
+  req.body.organismID = organismResults['insertId'];
+  console.log(req.body.organismID);
       
-      processFasta(connection, req.body, res, dbEnzymes, dbMods)
-      .then(function() {
-        console.log("nearly done");
-      });
-    });
-    
-    return;
-  });
+  let return_status = {};
+  return_status.msg = "Process complete";
+  return_status.status = 200;
+  res.send(return_status);
 });
 
 router.post('/delete', function(req, res, next) {
-  var connection = mysql.createConnection({
+  let connection = new mysql({
     host     : req.body.server_address,
     port     : req.body.server_port,
     user     : req.body.server_username,
@@ -228,244 +163,170 @@ router.post('/delete', function(req, res, next) {
     multipleStatements: true
   });
   let query = 'SET FOREIGN_KEY_CHECKS = 0; DROP TABLE modMap; DROP TABLE unimod; DROP TABLE peptides; DROP TABLE sequences; DROP TABLE proteins; DROP TABLE organisms; DROP TABLE enzymes; SET FOREIGN_KEY_CHECKS = 1;';
-  connection.query(query, function (error, results, fields) {
-    if (error) {
-      console.log(error);
-      res.send({status: 400});
-      return;
-    };
-    res.send({status: 200});
-  });
+  connection.query(query);
+  res.send({status: 200});
 });
 
 router.post('/dbcreate', function(req, res, next) {
   // Setting up the connection to MySQL
   console.log(req.body);
-  var connection = mysql.createConnection({
+  let connection = new mysql({
     host     : req.body.server_address,
     port     : req.body.server_port,
     user     : req.body.server_username,
     password : req.body.server_password,
     database : 'pepDB'
   });
-   
-  connection.connect(function(err) {
-    // We start with a failing message and progressively make it successful
-    let return_status = {
-      'msg':'Error connecting',
-      'code': 400
-    }
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      res.send(return_status);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
 
-    // Let's first create our peptides table
-    let peptides_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`peptides\` (
-      \`pepID\` INT NOT NULL AUTO_INCREMENT,
-      \`mass\` VARCHAR(45) NOT NULL,
-      \`missed_cleavages\` INT NOT NULL,
-      \`enzymeID\` INT NOT NULL,
-      \`seqID\` INT NOT NULL,
-      \`organismID\` INT NOT NULL,
-      PRIMARY KEY (\`pepID\`, \`mass\`),
-      UNIQUE INDEX \`pepID_UNIQUE\` (\`pepID\` ASC))`;
-    if(!runCreationCommand(connection, peptides_creation_command)) {
-      return_status.msg = 'Error creating peptides table';
-      res.send(return_status);
-    }
-    console.log('Created peptides table')
+  // Let's first create our peptides table
+  let peptides_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`peptides\` (
+    \`pepID\` INT NOT NULL AUTO_INCREMENT,
+    \`mass\` VARCHAR(45) NOT NULL,
+    \`missed_cleavages\` INT NOT NULL,
+    \`enzymeID\` INT NOT NULL,
+    \`seqID\` INT NOT NULL,
+    \`organismID\` INT NOT NULL,
+    PRIMARY KEY (\`pepID\`, \`mass\`),
+    UNIQUE INDEX \`pepID_UNIQUE\` (\`pepID\` ASC))`;
+  connection.query(peptides_creation_command);
+  console.log('Created peptides table')
 
-    // Now let's create the sequences table
-    let sequences_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`sequences\` (
-      \`seqID\` INT NOT NULL AUTO_INCREMENT,
-      \`sequence\` VARCHAR(100) NOT NULL,
-      \`start\` INT NOT NULL,
-      \`end\` INT NOT NULL,
-      \`length\` INT NOT NULL,
-      \`previousAA\` VARCHAR(1) NOT NULL,
-      \`nextAA\` VARCHAR(1) NOT NULL,
-      \`protID\` INT NOT NULL,
-      PRIMARY KEY (\`seqID\`),
-      UNIQUE INDEX \`seqID_UNIQUE\` (\`seqID\` ASC))`;
+  // Now let's create the sequences table
+  let sequences_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`sequences\` (
+    \`seqID\` INT NOT NULL AUTO_INCREMENT,
+    \`sequence\` VARCHAR(100) NOT NULL,
+    \`start\` INT NOT NULL,
+    \`end\` INT NOT NULL,
+    \`length\` INT NOT NULL,
+    \`previousAA\` VARCHAR(1) NOT NULL,
+    \`nextAA\` VARCHAR(1) NOT NULL,
+    \`protID\` INT NOT NULL,
+    PRIMARY KEY (\`seqID\`),
+    UNIQUE INDEX \`seqID_UNIQUE\` (\`seqID\` ASC))`;
 
-    if(!runCreationCommand(connection, sequences_creation_command)) {
-      return_status.msg = 'Error creating sequences table';
-      res.send(return_status);
-    }
-    console.log('Created sequences table');
+  connection.query(sequences_creation_command);
+  console.log('Created sequences table');
 
-    // Now let's create the proteins table
-    let proteins_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`proteins\` (
-      \`protID\` INT NOT NULL AUTO_INCREMENT,
-      \`accession\` VARCHAR(128) NOT NULL,
-      \`description\` TEXT NULL,
-      PRIMARY KEY (\`protID\`),
-      UNIQUE INDEX \`protID_UNIQUE\` (\`protID\` ASC))`;
+  // Now let's create the proteins table
+  let proteins_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`proteins\` (
+    \`protID\` INT NOT NULL AUTO_INCREMENT,
+    \`accession\` VARCHAR(128) NOT NULL,
+    \`description\` TEXT NULL,
+    PRIMARY KEY (\`protID\`),
+    UNIQUE INDEX \`protID_UNIQUE\` (\`protID\` ASC))`;
+  connection.query(proteins_creation_command);
+  console.log('Created proteins table');
 
-    if(!runCreationCommand(connection, proteins_creation_command)) {
-      return_status.msg = 'Error creating proteins table';
-      res.send(return_status);
-    }
-    console.log('Created proteins table');
+  // Now let's create the unimod table
+  let unimod_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`unimod\` (
+    \`modID\` INT NOT NULL AUTO_INCREMENT,
+    \`name\` VARCHAR(128) NOT NULL,
+    \`massShift\` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (\`modID\`),
+    UNIQUE INDEX \`modID_UNIQUE\` (\`modID\` ASC))`;
+  connection.query(unimod_creation_command);
+  console.log('Created unimod table');
 
-    // Now let's create the unimod table
-    let unimod_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`unimod\` (
-      \`modID\` INT NOT NULL AUTO_INCREMENT,
-      \`name\` VARCHAR(128) NOT NULL,
-      \`massShift\` VARCHAR(45) NOT NULL,
-      PRIMARY KEY (\`modID\`),
-      UNIQUE INDEX \`modID_UNIQUE\` (\`modID\` ASC))`;
-    
-    if(!runCreationCommand(connection, unimod_creation_command)) {
-      return_status.msg = 'Error creating unimod table';
-      res.send(return_status);
-    }
-    console.log('Created unimod table');
+  // Now let's create the modMap table
+  let modMap_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`modMap\` (
+    \`pepID\` INT NOT NULL,
+    \`modID\` INT NOT NULL,
+    PRIMARY KEY (\`pepID\`))`;
+  connection.query(modMap_creation_command);
+  console.log('Created modMap table');
 
-    // Now let's create the modMap table
-    let modMap_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`modMap\` (
-      \`pepID\` INT NOT NULL,
-      \`modID\` INT NOT NULL,
-      PRIMARY KEY (\`pepID\`))`;
-    
-    if(!runCreationCommand(connection, modMap_creation_command)) {
-      return_status.msg = 'Error creating modMap table';
-      res.send(return_status);
-    }
-    console.log('Created modMap table');
+  // Now let's create the enzymes table
+  let enzymes_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`enzymes\` (
+    \`enzymeID\` INT NOT NULL AUTO_INCREMENT,
+    \`enzyme\` VARCHAR(128) NULL,
+    PRIMARY KEY (\`enzymeID\`),
+    UNIQUE INDEX \`enzymeID_UNIQUE\` (\`enzymeID\` ASC))`;
+  connection.query(enzymes_creation_command);
+  console.log('Created enzymes table');
 
-    // Now let's create the enzymes table
-    let enzymes_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`enzymes\` (
-      \`enzymeID\` INT NOT NULL AUTO_INCREMENT,
-      \`enzyme\` VARCHAR(128) NULL,
-      PRIMARY KEY (\`enzymeID\`),
-      UNIQUE INDEX \`enzymeID_UNIQUE\` (\`enzymeID\` ASC))`;
-    
-    if(!runCreationCommand(connection, enzymes_creation_command)) {
-      return_status.msg = 'Error creating enzymes table';
-      res.send(return_status);
-    }
-    console.log('Created enzymes table');
+  // Now let's create the organisms table
+  let organisms_creation_command = 
+    `CREATE TABLE \`pepDB\`.\`organisms\` (
+    \`organismID\` INT NOT NULL AUTO_INCREMENT,
+    \`organism\` VARCHAR(128) NULL,
+    PRIMARY KEY (\`organismID\`),
+    UNIQUE INDEX \`organismID_UNIQUE\` (\`organismID\` ASC))`;
+  connection.query(organisms_creation_command);
+  console.log('Created organisms table');
 
-    // Now let's create the organisms table
-    let organisms_creation_command = 
-      `CREATE TABLE \`pepDB\`.\`organisms\` (
-      \`organismID\` INT NOT NULL AUTO_INCREMENT,
-      \`organism\` VARCHAR(128) NULL,
-      PRIMARY KEY (\`organismID\`),
-      UNIQUE INDEX \`organismID_UNIQUE\` (\`organismID\` ASC))`;
-    
-    if(!runCreationCommand(connection, organisms_creation_command)) {
-      return_status.msg = 'Error creating organisms table';
-      res.send(return_status);
-    }
-    console.log('Created organisms table');
-
-    // Adding the enzymes
-    let enzymes = require('../assets/enzymes').enzymes;
-    let enzymeQueryList = enzymes.map((e) => {
-      return '(\''+e+'\')';
-    });
-    let enzymeSQL = 'INSERT INTO enzymes (enzyme) VALUES '+enzymeQueryList.join(",");
-    runCreationCommand(connection, enzymeSQL);
-    console.log('Populated enzymes table');
-
-    // Adding the modifications w/ masses
-    let modNames = unimod.listMods();
-    let modQueryList = modNames.map((m) => {
-      let mod = unimod.getByName(m);
-      return '(\''+m+'\',\''+mod.mono_mass+'\')';
-    });
-    let modSQL = 'INSERT INTO unimod (name, massShift) VALUES '+modQueryList.join(',');
-    runCreationCommand(connection, modSQL);
-    console.log('Populated unimod table');
-
-    // Now we create the foreign keys
-    let fkSQL = 
-      `ALTER TABLE peptides
-      ADD CONSTRAINT FK_pepToSeq
-      FOREIGN KEY (seqID) REFERENCES sequences(seqID)`;
-    if(!runCreationCommand(connection, fkSQL)) {
-      return_status.msg = 'Error creating pepToSeq FK';
-      res.send(return_status);
-    }
-    console.log('Created peptides to sequences foreign key');
-
-    fkSQL = 
-    `ALTER TABLE peptides
-    ADD CONSTRAINT FK_pepToEnz
-    FOREIGN KEY (enzymeID) REFERENCES enzymes(enzymeID)`;
-    if(!runCreationCommand(connection, fkSQL)) {
-      return_status.msg = 'Error creating pepToEnz FK';
-      res.send(return_status);
-    }
-    console.log('Created peptides to enzymes foreign key');
-
-
-    fkSQL = 
-    `ALTER TABLE peptides
-    ADD CONSTRAINT FK_pepToOrg
-    FOREIGN KEY (organismID) REFERENCES organisms(organismID)`;
-    if(!runCreationCommand(connection, fkSQL)) {
-      return_status.msg = 'Error creating pepToOrg FK';
-      res.send(return_status);
-    }
-    console.log('Created peptides to organisms foreign key');
-
-    fkSQL = 
-    `ALTER TABLE sequences
-    ADD CONSTRAINT FK_seqToProt
-    FOREIGN KEY (protID) REFERENCES proteins(protID)`;
-    if(!runCreationCommand(connection, fkSQL)) {
-      return_status.msg = 'Error creating seqToProt FK';
-      res.send(return_status);
-    }
-    console.log('Created sequences to proteins foreign key');
-
-    fkSQL = 
-    `ALTER TABLE modMap
-    ADD CONSTRAINT FK_modToPep
-    FOREIGN KEY (pepID) REFERENCES peptides(pepID)`;
-    if(!runCreationCommand(connection, fkSQL)) {
-      return_status.msg = 'Error creating modToPep FK';
-      res.send(return_status);
-    }
-    console.log('Created modMap to peptides foreign key');
-
-    fkSQL = 
-    `ALTER TABLE modMap
-    ADD CONSTRAINT FK_modToMod
-    FOREIGN KEY (modID) REFERENCES unimod(modID)`;
-    if(!runCreationCommand(connection, fkSQL)) {
-      return_status.msg = 'Error creating modToMod FK';
-      res.send(return_status);
-    }
-    console.log('Created modMap to unimod foreign key');
-    
-    connection.end(function(err) {
-      if (err) {
-        return_status.msg = "Error closing connection";
-        return return_status;
-      }
-      return_status.msg = 'Database ready';
-      return_status.code = 200;
-      res.send(return_status);
-    });
-    
-    
-
-    
+  // Adding the enzymes
+  let enzymes = require('../assets/enzymes').enzymes;
+  let enzymeQueryList = enzymes.map((e) => {
+    return '(\''+e+'\')';
   });
+  let enzymeSQL = 'INSERT INTO enzymes (enzyme) VALUES '+enzymeQueryList.join(",");
+  connection.query(enzymeSQL);
+  console.log('Populated enzymes table');
+
+  // Adding the modifications w/ masses
+  let modNames = unimod.listMods();
+  let modQueryList = modNames.map((m) => {
+    let mod = unimod.getByName(m);
+    return '(\''+m+'\',\''+mod.mono_mass+'\')';
+  });
+  let modSQL = 'INSERT INTO unimod (name, massShift) VALUES '+modQueryList.join(',');
+  connection.query(modSQL);
+  console.log('Populated unimod table');
+
+  // Now we create the foreign keys
+  let fkSQL = 
+    `ALTER TABLE peptides
+    ADD CONSTRAINT FK_pepToSeq
+    FOREIGN KEY (seqID) REFERENCES sequences(seqID)`;
+  connection.query(fkSQL);
+  console.log('Created peptides to sequences foreign key');
+
+  fkSQL = 
+  `ALTER TABLE peptides
+  ADD CONSTRAINT FK_pepToEnz
+  FOREIGN KEY (enzymeID) REFERENCES enzymes(enzymeID)`;
+  connection.query(fkSQL);
+  console.log('Created peptides to enzymes foreign key');
+
+
+  fkSQL = 
+  `ALTER TABLE peptides
+  ADD CONSTRAINT FK_pepToOrg
+  FOREIGN KEY (organismID) REFERENCES organisms(organismID)`;
+  connection.query(fkSQL);
+  console.log('Created peptides to organisms foreign key');
+
+  fkSQL = 
+  `ALTER TABLE sequences
+  ADD CONSTRAINT FK_seqToProt
+  FOREIGN KEY (protID) REFERENCES proteins(protID)`;
+  connection.query(fkSQL);
+  console.log('Created sequences to proteins foreign key');
+
+  fkSQL = 
+  `ALTER TABLE modMap
+  ADD CONSTRAINT FK_modToPep
+  FOREIGN KEY (pepID) REFERENCES peptides(pepID)`;
+  connection.query(fkSQL);
+  console.log('Created modMap to peptides foreign key');
+
+  fkSQL = 
+  `ALTER TABLE modMap
+  ADD CONSTRAINT FK_modToMod
+  FOREIGN KEY (modID) REFERENCES unimod(modID)`;
+  connection.query(fkSQL);
+  console.log('Created modMap to unimod foreign key');
+  
+  let return_status={};
+  return_status.msg = 'Database ready';
+  return_status.code = 200;
+  res.send(return_status);
 });
 
 module.exports = router;
